2b672f90a81301b217ef6d40d189f828b4f637d9
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-09-15 19:09:58 +0330

    Range Of Values in sql

---
+++ Range Of Values.sql
@@ -0,0 +1,38 @@
+SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+  FROM [master].[dbo].[Products] where [UnitPrice] between '18' and '22';
+
+
+SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+  FROM [master].[dbo].[Products] where [SupplierID] in(1,2,3,23);
+
+
+
+SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+  FROM [master].[dbo].[Products] where [UnitPrice] in(18,23,22);
